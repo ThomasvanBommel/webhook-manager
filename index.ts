f91c89501a6c2db027f47c5c26c4ead39749d890
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-03-16 21:37:41 -0300

    Now listens for requests on all interfaces (port 8080)

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 import express from "express";
-// const express = require("express");
 
-// const app = express();
+const app = express();
 
-// app.use
+app.use(express.json());
 
-console.log("Hello!");
+app.all("*", (req, res) => {
+    if(req.url === "/favicon.ico") return;
+    
+    console.log("----------------------")
+    console.log("ID: ", req.ip, req.hostname);
+    console.log("URL: ", req.url, req.params);
+    console.log("Headers: ", req.headers);
+    console.log("Some JSON: ",  req.body);
+
+    res.send("Welcome!");
+});
+
+app.listen(8080, () => 
+    console.log("Listening @ http://192.168.1.181:8080")
+);
